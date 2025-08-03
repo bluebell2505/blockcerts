f6737cb71188c6c73cce94b229971bfc8f65bcc0
Abhinav Mehta <abhinavmehta374@gmail.com>
2025-08-03 19:43:05 +0530

    Resolve hardhat.config.js merge conflict

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.28",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
